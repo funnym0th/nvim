653998f26b76d6d7abcb6cc35e0bed41d5ebee56
Camilo <mothbms@gmail.com>
2025-04-20 14:56:13 -0400

    modificar archivos y añadir plugin lualine.lua

--- lua/moth/plugins.lua
+++ lua/moth/plugins.lua
@@ -1,5 +1,10 @@
 return {
-    { "itchyny/lightline.vim" },
-    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-    { "shinchu/lightline-gruvbox.vim" },
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup(require('plugins.lualine'))
+        end
+    }
 }
--- lua/moth/theme.lua
+++ lua/moth/theme.lua
@@ -1,27 +1,3 @@
--- lightline.vim settings
-vim.g.lightline = {
-    colorscheme = 'gruvbox',
-    active = {
-        left = {
-            { 'mode', 'paste' },
-            { 'gitbranch', 'readonly', 'filename', 'modified' },
-        }
-    },
-    mode_map = {
-        ['n'] = 'Normal',
-        ['i'] = 'Insert',
-        ['R'] = 'Replace',
-        ['v'] = 'Visual',
-        ['V'] = 'Visual Line',
-        ['\x16'] = 'Visual Block',
-        ['c'] = 'Command',
-        ['s'] = 'Select',
-        ['S'] = 'Select Line',
-        ['\x13'] = 'Select Block',
-        ['t'] = 'Terminal',
-    }
-}
-
 -- Gruvbox settings
 require("gruvbox").setup({
   terminal_colors = true,
